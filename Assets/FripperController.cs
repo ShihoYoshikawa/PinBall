27a65d71edd99d26f0fd28d157d4fee7986728e8
吉川志穂 <oceanblue0066cc@gmail.com>
2017-04-04 20:14:22 +0900

    Lesson5の課題、発展課題を新規作成
    Lesson5の課題、発展課題を新規作成

---
+++ Assets/FripperController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FripperController : MonoBehaviour
+{
+    //HingiJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    //スクリーンのwidthの半分の値
+    private int harfScreenWidth;
+
+    // Use this for initialization
+    void Start()
+    {
+        //HinjiJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+
+        harfScreenWidth =
+            Screen.width / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Unityエディタ上ではない場合は、タッチでフリッパーの操作を行う
+        if (!Application.isEditor)
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.position.x < harfScreenWidth && touch.phase == TouchPhase.Began && tag == "LeftFripperTag")
+                {
+                    SetAngle(this.flickAngle);
+                }
+                else if (touch.position.x > harfScreenWidth && touch.phase == TouchPhase.Began && tag == "RightFripperTag")
+                {
+                    SetAngle(this.flickAngle);
+                }
+                else if (touch.position.x < harfScreenWidth && touch.phase == TouchPhase.Ended && tag == "LeftFripperTag")
+                {
+                    SetAngle(this.defaultAngle);
+                }
+                else if (touch.position.x > harfScreenWidth && touch.phase == TouchPhase.Ended && tag == "RightFripperTag")
+                {
+                    SetAngle(this.defaultAngle);
+                }
+            }
+        }
+        else
+        {
+            //左矢印キーを押した時左フリッパーを動かす
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+            {
+                SetAngle(this.flickAngle);
+            }
+            //右矢印キーを押した時右フリッパーを動かす
+            if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+            {
+                SetAngle(this.flickAngle);
+            }
+
+            //矢印キー離された時フリッパーを元に戻す
+            if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                SetAngle(this.defaultAngle);
+            }
+        }
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
---
+++ Assets/PointCounter.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PointCounter : MonoBehaviour
+{
+    // Materialを入れる
+    Material myMaterial;
+
+    // Emissionの最小値
+    private float minEmission = 0.3f;
+    // Emissionの強度
+    private float magEmission = 2.0f;
+    // 角度
+    private int degree = 0;
+    //発光速度
+    private int speed = 10;
+    // ターゲットのデフォルトの色
+    Color defaultColor = Color.white;
+    //得点を表示するテキスト
+    private Text pointText;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のPointTextオブジェクトを取得
+        GameObject pointTextObject = GameObject.Find("PointText");
+        this.pointText = pointTextObject.GetComponent<Text>();
+    }
+
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        CountPoint();
+    }
+
+    // 得点を数える関数
+    void CountPoint()
+    {
+        int point = int.Parse(pointText.text);
+        if (tag == "SmallStarTag")
+        {
+            point += 10;
+            pointText.text = point.ToString();
+        }
+        else if (tag == "LargeStarTag")
+        {
+            point += 20;
+            pointText.text = point.ToString();
+        }
+        else if (tag == "SmallCloudTag")
+        {
+            point -= 5;
+            pointText.text = point.ToString();
+        }
+        else if (tag == "LargeCloudTag")
+        {
+            point -= 10;
+            pointText.text = point.ToString();
+        }
+    }
+}
